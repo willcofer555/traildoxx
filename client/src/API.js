5059755d30ffa742da065af5fced0a2feddfd2da
William Cofer <willcofer@gmail.com>
2020-01-04 11:13:33 -0600

    trying routes on uri

--- client/src/API.js
+++ client/src/API.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000/api/v1/messages' : 'https://api.traildocs.xyz/api/v1/messages';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000/api/v1/messages' : 'mongodb://willcofer:floating22@ds359118.mlab.com:59118/heroku_xjlhpst3/api/v1/messages';
 
 
 
