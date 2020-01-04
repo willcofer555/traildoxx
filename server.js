c16c7344d092974a279deeee808279eda5651bb5
William Cofer <willcofer@gmail.com>
2020-01-04 08:02:20 -0600

    ./ to file directory for client/build

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-  app.use('/', express.static(path.join(__dirname, "client/build")));
+  app.use('/', express.static(path.join(__dirname, "./client/build")));
 
   app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html')); // relative path
+        res.sendFile(path.join(__dirname, './client/build', 'index.html')); // relative path
     });
 }
 
