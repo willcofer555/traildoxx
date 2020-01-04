f44a14da88607e206fb59b6ac603af020e12c3e4
William Cofer <willcofer@gmail.com>
2020-01-04 09:53:59 -0600

    switch client/build

--- server.js
+++ server.js
@@ -36,11 +36,10 @@ app.use(middlewares.errorHandler);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build', 'index.html')); // relative path
-    });
 }
+app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, './client/build', 'index.html')); // relative path
+  });
 
 
 app.listen(PORT, () => {
