dc8c92c4c2ac3381ae1a00d5c568ee4c84d4e27e
William Cofer <willcofer@gmail.com>
2020-01-04 08:28:15 -0600

    changed directory for dep

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-  app.use('/', express.static(path.join(__dirname, "./client/build")));
+  app.use('/', express.static(path.join(__dirname, "client/build")));
 
   app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, './client/build', 'index.html')); // relative path
